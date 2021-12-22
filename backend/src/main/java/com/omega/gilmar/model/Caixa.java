5bf4a48f8255c1c18f487da4f4f9dee159f72239
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-12-22 20:02:10 -0300

    backend finanlizada

--- backend/src/main/java/com/omega/gilmar/model/Caixa.java
+++ backend/src/main/java/com/omega/gilmar/model/Caixa.java
@@ -1,5 +1,25 @@
 package com.omega.gilmar.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "caixa")
 public class Caixa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	@NotBlank
+	private String descricao;
 }
